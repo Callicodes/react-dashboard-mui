0c96d97405988cf12ac46328bd7377deb4b25118
Aurelius Calliste <aureliuscalliste@Aureliuss-iMac.local>
2025-12-14 22:40:00 +0000

    feat: implement Formik user form with validation (WIP)

---
+++ src/scenes/form/index.js
@@ -0,0 +1,67 @@
+import { Box, Button, TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/Header";
+
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
+};
+
+const phoneRegExp =
+  /^\((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const userSchema = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  contact: Yup.string()
+    .matches(phoneRegExp, "Phone numbers is not valid")
+    .required("Required"),
+  address1: Yup.string().required("Required"),
+  address2: Yup.string().required("Required"),
+});
+
+const Form = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="CREATE USER" subtitle="Create a New User Profile" />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={userSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+            ></Box>
+          </form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+export default Form;
